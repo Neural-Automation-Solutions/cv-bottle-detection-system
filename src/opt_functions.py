4c07f936e8b97d3f11795172d496d120eb3cc34a
yiannisha <hadjiyiannis.3@gmail.com>
2024-04-14 17:05:53 +0300

    Update package optimizer function

--- src/opt_functions.py
+++ src/opt_functions.py
@@ -1,131 +1,159 @@
-from src.detector import PackageDetector, BottleDetector
-import numpy as np
-import cv2
-from PIL import Image
 import os
+import json
+import cv2
+import numpy as np
 
+from src.detector import PackageDetector, BottleDetector
+from src.optimizer import Optimizer
+# from PIL import Image
 
-def package_optimization(alpha, beta, gamma):
+from typing import Tuple
+
+def package_optimization(
+        data_dir: str,
+        annotation_file: str,
+        height: int = 480,
+        width: int = 640,
+        n_iter: int = 10,
+        init_points: int = 5,
+    ) -> Tuple[float, float, float]:
     '''
-    The function that will be passed into the optimizer to find the proper
-    alpha, beta, and gamma parameters for the package.
-
-    :param alpha: The first parameter controlling the contrast of the image.
-    Allowed values are alpha >= 0.
-    Use 0 <= alpha < 1 to decrease the contrast of the image.
-    Use alpha > 1 to increase the contrast of the image.
+    Optimizes a PackageDetector object to detect packages in images.
     
-    :param beta: The second parameter controlling the brightness of the image.
-    Allowed values are -127 <= beta <= 127.
+    :param data_dir: The directory containing the images to be used for optimization.
+    :param annotation_file: The JSON file containing the annotations for the images in the data_dir.
     
-    :param gamma: The third parameter controlling the gamma correction (exposure) of the image.
-    Allowed values are gamma >= 1.
-
-    :return: The ration of the bottles the model detected devided than the bottles it should've
-    detected
-    '''
-
-    # Hard code the number of packages the given video contains
-    total_packages = 6
-
-    # Setting the directory that contain the images
-    video_path = os.path.join("./data/cropped_video.mp4")
-
-    # Setting the capture object
-    cap = cv2.VideoCapture(video_path)
-
-    # Setting the size of the frames 
-    ret, frame = cap.read()
-    width, height = frame.shape[:2]
-
-    # Initialize the detector object
-    package_detector = PackageDetector(
-        input_shape=(width, height, 3),
-        padding={
-            'top': 30,
-            'bottom': 460,
-            'left': 100,
-            'right': 600,
-        },
-        alpha=alpha,
-        beta=beta,
-        gamma=gamma
-    )
-
-    is_package = False
-    detected = 0
-    while (ret):
-        package = package_detector.detect_package(frame, _preproc=True)
-
-        # Condition to prevent the same package to be counted twice
-        if package is not None:
-            if not is_package:
-                is_package = True
-                detected += 1
-        else:
-            if is_package:
-                is_package = False
-
-        ret, frame = cap.read()
-
-    cap.release()
-
-    return detected/total_packages
-
-
-def bottle_optimization(alpha: float, beta: float, gamma: float) -> float:
-    '''
-    The function that will be passed into the optimizer to find the proper
-    alpha, beta, and gamma parameters for the bottles.
-
-    :param alpha: The first parameter controlling the contrast of the image.
-    Allowed values are alpha >= 0.
-    Use 0 <= alpha < 1 to decrease the contrast of the image.
-    Use alpha > 1 to increase the contrast of the image.
-    
-    :param beta: The second parameter controlling the brightness of the image.
-    Allowed values are -127 <= beta <= 127.
-    
-    :param gamma: The third parameter controlling the gamma correction (exposure) of the image.
-    Allowed values are gamma >= 1.
-
-    :return: The ration of the bottles the model detected devided than the bottles it should've
-    detected
+    :return: The optimized alpha, beta, and gamma parameters for the PackageDetector object.
     '''
 
-    # The number of bottles expected at every box
-    num_bottles = 12
-
-    # Initialize the detector object
-    bottle_detector = BottleDetector(
-        num_bottles=num_bottles,
-        alpha=alpha,
-        beta=beta,
-        gamma=gamma
-    )
-
-    # Setting the directory that contain the images
-    folder_path = os.path.join("./data")
+    # load annotations
+    with open(annotation_file, 'r') as f:
+        annotations = json.load(f)
     
-    # Iterating over all images
-    non_image_path = 0
-    bottles_detected = 0
-    for img_path in os.listdir(folder_path):
-        if not img_path.endswith("jpg"):
-            non_image_path += 1
-            continue
+    def loss_function(alpha: float, beta: float, gamma: float) -> float:
+        '''
+        The function that will be passed into the optimizer to find the proper
+        alpha, beta, and gamma parameters for the packages.
         
-        # Covnerting the image to np array
-        image_path = os.path.join(folder_path, img_path)
-        image = Image.open(image_path)
-        img = np.array(image)
+        :param alpha: The first parameter controlling the contrast of the image.
+        Allowed values are alpha >= 0.
+        Use 0 <= alpha < 1 to decrease the contrast of the image.
+        Use alpha > 1 to increase the contrast of the image.
+        
+        :param beta: The second parameter controlling the brightness of the image.
+        Allowed values are -300 <= beta <= 300.
+        
+        :param gamma: The third parameter controlling the gamma correction (exposure) of the image.
+        Allowed values are gamma >= 1.
+        
+        :return: The ration of the packages the model detected devided than the packages it should've
+        detected
+        '''
+        
+        # Initialize the detector object
+        package_detector = PackageDetector(
+            input_shape=(width, height, 3),
+            padding={
+                'top': 110,
+                'bottom': 410,
+                'left': 140,
+                'right': 530,
+            },
+            alpha=alpha,
+            beta=beta,
+            gamma=gamma
+        )
+        
+        correct_detection = 0
+        for annotation in annotations:
+            
+            path = os.path.join(data_dir, annotation['filename'])
+            
+            # read image
+            img = cv2.imread(os.path.join(data_dir, annotation['filename']))
+            
+            # detect package
+            package = package_detector.detect_package(img, _preproc=True)
+            
+            # Condition to prevent the same package to be counted twice
+            if package is not None and annotation['package']:
+                correct_detection += 1
+            elif package is None and not annotation['package']:
+                correct_detection += 1
+        
+        return correct_detection/len(annotations)
 
-        # Calculating the bottles in the image
-        bottles = bottle_detector.detect_bottles(img, _preproc=True)
-        bottles_detected += bottles
+    # Initialize the optimizer
+    optimizer = Optimizer(
+        function=loss_function,
+        pbounds={
+            'alpha': (0, 10),
+            'beta': (-300, 300),
+            'gamma': (1, 10)
+        }
+    )
+    
+    # Run the optimizer
+    optimizer.maximize(n_iter=n_iter, init_points=init_points)
+    
+    # get the optimal parameters
+    optimal_params = optimizer.get_max()
+    
+    return optimal_params['alpha'], optimal_params['beta'], optimal_params['gamma']
 
-    # Finding the total bottles the model should have detected
-    with open(os.path.join(folder_path, "bottles.txt")) as f:
-        total_truth_bottles = sum([int(num_b) for num_b in f.readlines()])    
+# def bottle_optimization(alpha: float, beta: float, gamma: float) -> float:
+#     '''
+#     The function that will be passed into the optimizer to find the proper
+#     alpha, beta, and gamma parameters for the bottles.
 
-    return bottles_detected/total_truth_bottles
+#     :param alpha: The first parameter controlling the contrast of the image.
+#     Allowed values are alpha >= 0.
+#     Use 0 <= alpha < 1 to decrease the contrast of the image.
+#     Use alpha > 1 to increase the contrast of the image.
+    
+#     :param beta: The second parameter controlling the brightness of the image.
+#     Allowed values are -127 <= beta <= 127.
+    
+#     :param gamma: The third parameter controlling the gamma correction (exposure) of the image.
+#     Allowed values are gamma >= 1.
+
+#     :return: The ration of the bottles the model detected devided than the bottles it should've
+#     detected
+#     '''
+
+#     # The number of bottles expected at every box
+#     num_bottles = 12
+
+#     # Initialize the detector object
+#     bottle_detector = BottleDetector(
+#         num_bottles=num_bottles,
+#         alpha=alpha,
+#         beta=beta,
+#         gamma=gamma
+#     )
+
+#     # Setting the directory that contain the images
+#     folder_path = os.path.join("./data")
+    
+#     # Iterating over all images
+#     non_image_path = 0
+#     bottles_detected = 0
+#     for img_path in os.listdir(folder_path):
+#         if not img_path.endswith("jpg"):
+#             non_image_path += 1
+#             continue
+        
+#         # Covnerting the image to np array
+#         image_path = os.path.join(folder_path, img_path)
+#         image = Image.open(image_path)
+#         img = np.array(image)
+
+#         # Calculating the bottles in the image
+#         bottles = bottle_detector.detect_bottles(img, _preproc=True)
+#         bottles_detected += bottles
+
+#     # Finding the total bottles the model should have detected
+#     with open(os.path.join(folder_path, "bottles.txt")) as f:
+#         total_truth_bottles = sum([int(num_b) for num_b in f.readlines()])    
+
+#     return bottles_detected/total_truth_bottles
